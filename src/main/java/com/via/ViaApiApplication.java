6c046d0ee452f3f38521170c6dac4ecc12c5e336
Felipe Praxedes <felipe_zanata@outlook.com>
2023-02-21 21:42:48 -0300

    Metricas de uso, Prometheus e Grafana

--- src/main/java/com/via/ViaApiApplication.java
+++ src/main/java/com/via/ViaApiApplication.java
@@ -2,9 +2,11 @@ package com.via;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.via.data.FiliaisUtil;
 
+@EnableWebMvc
 @SpringBootApplication
 public class ViaApiApplication {
 
